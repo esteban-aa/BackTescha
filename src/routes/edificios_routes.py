534be696848fbcba067a7196ae22a0e792b9acc3
yael_ <yael_op@tesch.edu.mx>
2025-06-18 22:42:52 -0600

    Cambios

--- src/routes/edificios_routes.py
+++ src/routes/edificios_routes.py
@@ -1,9 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from src.database.db import db
 
 from src.controller.edificios_con import (
     obtener_edificios_controller,
     obtener_edificio_controller
 )
+
+edificios = db["edificios"]
 
 router = Blueprint("edificios", __name__)
 
@@ -14,3 +17,13 @@ def obtener_edificios_route():
 @router.route("/<string:id>", methods=["GET"])
 def obtener_edificio_route(id):
     return obtener_edificio_controller(id)
+
+# Nueva ruta para obtener edificio por nombre
+@router.route("/nombre/<string:nombre>", methods=["GET"])
+def get_edificio_por_nombre(nombre):
+    edificio = edificios.find_one({"nombre": nombre})
+    if edificio:
+        edificio.pop("_id", None)
+        return jsonify(edificio), 200
+    else:
+        return jsonify({"error": "Edificio no encontrado"}), 404
